65d3beff7803b6a8316e590e20e2f7ac04c42a92
Rmgp09 <rgonzalespacherrez@gmail.com>
2022-10-08 08:57:11 -0500

    hice la barra de busqueda en la pagina home

--- lib/src/view/pages/app/home/home_page.dart
+++ lib/src/view/pages/app/home/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:library_book_app/src/view/shared/components/utils_components.dart';
+import 'package:library_book_app/src/shared/sc_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,7 +9,35 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: UtilsComponents.getBoxDecoration(),
+     /// decoration: UtilsComponents.getBoxDecoration(),
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(
+        left : 8.0,
+        right: 8.0,
+      ),
+      height: 50.0,
+      decoration: BoxDecoration(
+        color: Colors.white10,
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+
+  child: TextField(
+    cursorColor: Colors.grey,
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+            CupertinoIcons.search_circle,
+          size:40.0,
+        color: Colors.grey,
+        ),
+    suffixIcon: Icon(
+      Icons.tune,
+     color: Colors.grey,
+      size: 28.0,
+    ),
+  border: InputBorder.none,
+        hintText: 'Buscar'
+      ),
+    ),
     );
   }
 }
