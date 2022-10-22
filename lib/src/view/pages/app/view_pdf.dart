7c4fe3fdf34ac1ff7574443c4276b799f2d29f84
NILBER <nilberadrian00@gmail.com>
2022-10-22 14:06:55 -0500

    agregue otro icono(arrow_back_ios_new) y hice que apreciera en Appbar del lado leading

--- lib/src/view/pages/app/view_pdf.dart
+++ lib/src/view/pages/app/view_pdf.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -14,14 +13,12 @@ class MyApp extends StatelessWidget {
       title: 'Lectura',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+        cardColor: Colors.black,
       ),
       home: MyHomePage(title: 'Pagina de lectura'),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -45,17 +42,19 @@ class _MyHomePageState extends State<MyHomePage> {
     return SafeArea(
         child: Scaffold(
           body: SfPdfViewer.network(
-              'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+              'http://www.ataun.eus/BIBLIOTECAGRATUITA/Cl%C3%A1sicos%20en%20Espa%C3%B1ol/Charles%20Perrault/Caperucita%20%20Roja.pdf',
               controller: _pdfViewerController,
               key: _pdfViewerStateKey),
           appBar: AppBar(
+            leading: Builder(
+              builder: (BuildContext context) {
+                return IconButton(
+                  icon: const Icon(Icons.arrow_back_ios_new),
+                  onPressed: () { },
+
+                );
+              },),
             actions: <Widget>[
-
-              IconButton(
-                icon: Icon(Icons.arrow_back),
-                onPressed: () {
-                },),
-
               IconButton(
                   onPressed: () {
                     _pdfViewerController.jumpToPage(1);
@@ -86,4 +85,4 @@ class _MyHomePageState extends State<MyHomePage> {
         ));
   }
 }
-
+//
