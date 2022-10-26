b3b0034ed1f3cf854e1fc7c932b09cc892c81045
NILBER <nilberadrian00@gmail.com>
2022-10-25 20:32:56 -0500

    agregue otro icono(arrow_back_ios_new) y hice que apreciera en Appbar del lado leading

--- lib/src/view/pages/app/view_pdf.dart
+++ lib/src/view/pages/app/view_pdf.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -31,6 +32,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late PdfViewerController _pdfViewerController;
   final GlobalKey<SfPdfViewerState> _pdfViewerStateKey = GlobalKey();
+
   @override
   void initState() {
     _pdfViewerController = PdfViewerController();
@@ -41,47 +43,47 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          body: SfPdfViewer.network(
-              'http://www.ataun.eus/BIBLIOTECAGRATUITA/Cl%C3%A1sicos%20en%20Espa%C3%B1ol/Charles%20Perrault/Caperucita%20%20Roja.pdf',
-              controller: _pdfViewerController,
-              key: _pdfViewerStateKey),
-          appBar: AppBar(
-            leading: Builder(
-              builder: (BuildContext context) {
-                return IconButton(
-                  icon: const Icon(Icons.arrow_back_ios_new),
-                  onPressed: () { },
-
-                );
-              },),
-            actions: <Widget>[
-              IconButton(
-                  onPressed: () {
-                    _pdfViewerController.jumpToPage(1);
-                  },
-                  icon: Icon(
-                    Icons.arrow_drop_down_circle,
-                    color: Colors.white,
-                  )),
-
-              IconButton(
-                icon: Icon(Icons.settings,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  // do something
-                },),
-
-              IconButton(
-                  onPressed: () {
-                    _pdfViewerController.zoomLevel = 1.25;
-                  },
-                  icon: Icon(
-                    Icons.zoom_in,
-                    color: Colors.white,
-                  ))
-            ],
+      body: SfPdfViewer.network(
+          'http://www.ataun.eus/BIBLIOTECAGRATUITA/Cl%C3%A1sicos%20en%20Espa%C3%B1ol/Charles%20Perrault/Caperucita%20%20Roja.pdf',
+          controller: _pdfViewerController,
+          key: _pdfViewerStateKey),
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.arrow_back_ios_new),
+              onPressed: () {},
+            );
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                _pdfViewerController.jumpToPage(1);
+              },
+              icon: Icon(
+                Icons.arrow_drop_down_circle,
+                color: Colors.white,
+              )),
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // do something
+            },
           ),
-        ));
+          IconButton(
+              onPressed: () {
+                _pdfViewerController.zoomLevel = 1.25;
+              },
+              icon: Icon(
+                Icons.zoom_in,
+                color: Colors.white,
+              ))
+        ],
+      ),
+    ));
   }
 }
