2604775b446ce0a8cd5ddaa68e74525ced842a6f
spacecodee <spacecodee@gmail.com>
2022-10-25 20:47:27 -0500

    Nothing important

--- lib/src/view/pages/app/home/info_page.dart
+++ lib/src/view/pages/app/home/info_page.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:library_book_app/src/view/shared/components/homelibrary.dart';
-
 
 class InfoPage extends StatelessWidget {
   const InfoPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: 'home',
-        routes: <String,WidgetBuilder>{
-          'home'  : (BuildContext context) => homePage(),
-          'profile' : (BuildContext context) => homePage(),
-        }
-
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[],
+      ),
     );
   }
 }
