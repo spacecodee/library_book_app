d2f5156f5cdd763d75b13ab732dbb03c9befdcd9
spacecodee <spacecodee@gmail.com>
2022-11-08 10:53:54 -0500

    Home page optimizing

--- lib/src/view/pages/app/home/home_page.dart
+++ lib/src/view/pages/app/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:library_book_app/src/shared/sc_colors.dart';
 import 'package:library_book_app/src/shared/sc_responsive.dart';
 import 'package:library_book_app/src/view/shared/widgets/texts/sc_input_search.dart';
+import 'package:library_book_app/src/view/shared/widgets/texts/sc_search_input.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -24,22 +25,12 @@ class HomePage extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // search bar
-              ScInputSearch(
-                onTap: () {
-                  print("Hello");
-                },
-                width: myResponsive.widthPercentage(80),
-                height: myResponsive.heightPercentage(5.5),
-                fontSize: myResponsive.widthPercentage(4),
-                hintText: 'Hello',
-                hintTextColor: SCColors.accentMaterial[600],
-                hintTextSize: myResponsive.widthPercentage(4),
-              ),
-            ],
+          ScSearchInput(
+            width: myResponsive.widthPercentage(80),
+            height: myResponsive.heightPercentage(5.4),
+            hintText: "Search your favorite book",
+            fontSize: myResponsive.widthPercentage(3),
+            hintTextSize: myResponsive.widthPercentage(4),
           ),
         ],
       ),
---
+++ lib/src/view/shared/widgets/texts/sc_search_input.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:library_book_app/src/shared/sc_colors.dart';
+import 'package:library_book_app/src/view/shared/widgets/texts/sc_input_search.dart';
+
+class ScSearchInput extends StatelessWidget {
+  final EdgeInsetsGeometry? padding;
+  final double width;
+  final double height;
+  final String hintText;
+  final Function()? onTap;
+  final double fontSize;
+  final double hintTextSize;
+
+  const ScSearchInput({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.hintText,
+    required this.fontSize,
+    required this.hintTextSize,
+    this.onTap,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ScInputSearch(
+          onTap: onTap,
+          width: width,
+          height: height,
+          fontSize: fontSize,
+          hintText: hintText,
+          hintTextColor: SCColors.accentMaterial[600],
+          hintTextSize: hintTextSize,
+        ),
+      ],
+    );
+  }
+}
